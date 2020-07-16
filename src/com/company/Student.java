cab364448f7ff79f8b7e0e4c0c6528b0bf9eaa97
grzehoo <grzehoo.wrzosek@gmail.com>
2020-07-16 07:54:52 +0100

    Added no argument Constructor with details

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -1,4 +1,29 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Student {
+
+    private String firstName;
+    private String lastName;
+    private int gradeYear;
+    private int studentID;
+    private String courses;
+    private static int tutitionBalance;
+    private int courseOfCost = 600;
+
+
+    public Student() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter student first name: ");
+        this.firstName = scanner.nextLine();
+
+        System.out.print("Enter student last name: ");
+        this.lastName = scanner.nextLine();
+
+        System.out.print("1 - Freshman\n2 - Sophmore\n3 - Junior\n2 - Senior\nEnter student class level: ");
+        this.gradeYear = scanner.nextInt();
+
+        System.out.println(this.firstName + " " + this.lastName + " " + this.gradeYear);
+    }
 }
--- src/com/company/StudentDatabaseApp.java
+++ src/com/company/StudentDatabaseApp.java
@@ -3,6 +3,7 @@ package com.company;
 public class StudentDatabaseApp {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Student student = new Student();
     }
 }
